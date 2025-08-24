5c274d09aad15da7d87bfb95939996b672823ce0
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-24 15:10:33 +0530

    type inference and function para annotation

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -14,3 +14,23 @@ console.log("Name:", myName);
 console.log("Favorite Number:", myFavNum);
 console.log("Have girlfriend:", have);
 */
+
+//! Type Inference
+
+let favTeacher = "Sharma Mam";
+let favNumber = 12;
+
+// console.log(favTeacher);
+
+//! Function Parameter Annotation
+
+// Regular Function
+function plusFive(val: number) {
+  return val + 5;
+}
+
+// Arrow Function
+const double = (a: number, b: number) => a * b;
+
+// console.log(plusFive(4));
+// console.log(double(4, 5));
