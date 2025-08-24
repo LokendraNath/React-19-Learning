cc5617b71536bc1d0ef6faf3fdd98d95fc91c4da
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-24 15:09:46 +0530

    annotation

---
+++ Typescript/index.ts
@@ -0,0 +1,16 @@
+//! Annotation Example
+
+//? String
+let myName: string = "Lokendra Nath";
+
+//? Number
+let myFavNum: number = 72;
+
+//? Boolean
+let HaveGf: boolean = false;
+
+/* 
+console.log("Name:", myName);
+console.log("Favorite Number:", myFavNum);
+console.log("Have girlfriend:", have);
+*/
