8ee5be0b776a8e16c52af113ce5cba5a18085e01
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-30 21:56:34 +0530

    example of react hook form

--- React Hook Form/src/App.tsx
+++ React Hook Form/src/App.tsx
@@ -1,7 +1,9 @@
+import { Form } from "./Component/Form";
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold text-blue-600">Hello MuderFucker</h1>
+      <Form />
     </div>
   );
 };
---
+++ React Hook Form/src/Component/Form.tsx
@@ -0,0 +1,75 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const Form = () => {
+  //* Register : is used to connect input fields to the form.
+  //* Handlesubmit : is a function to handle form submission.
+  //* errors: contain validation errors for the form.
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="name">Name :</label>
+        <input
+          style={{ border: "1px solid" }}
+          type="text"
+          id="name"
+          placeholder="Enter Name"
+          {...register("name", { required: "name is required" })}
+        />
+        {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
+      </div>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter Email"
+          {...register("email", {
+            required: "Enter Email",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Enter a valid email address",
+            },
+          })}
+        />
+        {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter Password"
+          {...register("password", {
+            required: "Enter password",
+            pattern: {
+              value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+              message:
+                "Password must be at least 8 characters and contain letters and numbers",
+            },
+          })}
+        />
+        {errors.password && (
+          <p style={{ color: "red" }}>{errors.password.message}</p>
+        )}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
