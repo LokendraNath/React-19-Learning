8eb792f78fe68bff6651d04a9048b882349204d9
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-01 13:58:50 +0530

    practice toggle dark mode

--- Popular Component Libraries/toggle theme/src/App.tsx
+++ Popular Component Libraries/toggle theme/src/App.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 const App = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-blue-600">Hello MuderFucker</h1>
+    <div className={`${theme === "light" ? "bg-white" : "bg-blue-950"}`}>
+      <h1
+        className={`text-2xl font-bold ${
+          theme === "light" ? "text-blue-800" : "text-white"
+        }  `}
+      >
+        Hello MuderFucker
+      </h1>
+      <button
+        onClick={toggleTheme}
+        className="bg-blue-600 text-white px-2 font-bold rounded-xl"
+      >
+        {theme === "light" ? "Turn Dark Mode" : "Turn Light Mode"}
+      </button>
     </div>
   );
 };
