29f4c13e3524d11405cbe9a276510ad5bea1aaa2
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-02 11:16:12 +0530

    delete

--- React 19 Features/src/App.tsx
+++ React 19 Features/src/App.tsx
@@ -1,13 +1,4 @@
-import { Suspense } from "react";
-import { Todo } from "./component/NEWxFetchData";
-// import { FetchTodo } from "./component/OLD-FetchTodo";
-// import { NEWxFetchData, Todo } from "./component/NEWxFetchData";
-
 const App = () => {
-  return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <Todo />
-    </Suspense>
-  );
+  return <div>Hello</div>;
 };
 export default App;
