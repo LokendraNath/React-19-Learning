3ae32c196febc791729a3ae5fcc64b9b0a0268bd
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-04 16:18:51 +0530

    Type Accertion/ Type Casting

---
+++ Typescript/06 Type Assertions/index.ts
@@ -0,0 +1,7 @@
+//? Type Accertion / Type Casting
+
+// Type accertion ka matlab hai ki typescript ko batana ki perticular item ka type kya hai
+
+let exm4: any = "lokendra";
+
+console.log((exm4 as string).toLocaleUpperCase()); // 👉 LOKENDRA
