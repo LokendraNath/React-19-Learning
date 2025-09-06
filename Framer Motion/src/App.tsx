2209c8303e951c102db1e06e88c97017a0fe46f8
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-06 17:04:30 +0530

    basic transformation of framer motion

--- Framer Motion/src/App.tsx
+++ Framer Motion/src/App.tsx
@@ -1,4 +1,16 @@
+import { motion } from "motion/react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-950">
+      <motion.div
+        // animate={{ x: -100, y: "50px" }}
+        // animate={{ rotateY: 50 }}
+        // animate={{ scaleY: 1.5 }}
+        // animate={{ skewY: 23 }}
+        className="h-50 w-50 bg-white rounded-lg"
+      />
+    </div>
+  );
 };
 export default App;
