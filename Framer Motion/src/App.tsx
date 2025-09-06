a24ab1cf61372553d49c5b8fc8873fd93b68db1d
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-06 17:27:31 +0530

    Transitions

--- Framer Motion/src/App.tsx
+++ Framer Motion/src/App.tsx
@@ -4,11 +4,18 @@ const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-950">
       <motion.div
+        //! Transformation
         // animate={{ x: -100, y: "50px" }}
         // animate={{ rotateY: 50 }}
         // animate={{ scaleY: 1.5 }}
         // animate={{ skewY: 23 }}
-        className="h-50 w-50 bg-white rounded-lg"
+
+        //! Transitions
+        initial={{ y: 0 }}
+        animate={{ y: 215 }}
+        // transition={{ delay: 3 }}
+        transition={{ delay: 2, duration: 1, ease: "easeInOut" }}
+        className="h-30 w-30 bg-white rounded-full"
       />
     </div>
   );
