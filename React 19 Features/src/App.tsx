790566be1a5bd591cadf823d9830399d3a87244e
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-06 12:01:55 +0530

    action attribute

--- React 19 Features/src/App.tsx
+++ React 19 Features/src/App.tsx
@@ -1,10 +1,13 @@
 // import { UseExample1 } from "./component/useExample1/Joke";
-import { UseExample2 } from "./component/useExample2/Posts";
+// import { UseExample2 } from "./component/useExample2/Posts";
+
+import Form from "./component/roadSideCoder/login";
 
 const App = () => {
   return (
     <div>
-      <UseExample2 />
+      {/* <UseExample2 /> */}
+      <Form />
     </div>
   );
 };
