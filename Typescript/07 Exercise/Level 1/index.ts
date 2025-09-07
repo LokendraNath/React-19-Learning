8faa1bc5b0cc53226b7d5a3e9f7f3dfae4de36ba
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-07 10:25:57 +0530

    complete level 1 ts challenge

---
+++ Typescript/07 Exercise/Level 1/index.ts
@@ -0,0 +1,21 @@
+//! 1. Type Annotations:
+const greetExample = (name: string) => {
+  return `Hello , ${name}`;
+};
+// console.log(greetExample("Lokendra"));
+
+//! 2. Interfaces:
+interface UserExample {
+  id: number;
+  email: string;
+  isActive: boolean;
+}
+
+const myUser: UserExample = {
+  id: 2,
+  email: "example1@gmail.com",
+  isActive: true,
+};
+
+//! Arrays & Types:
+const fruits: string[] = ["Banana", "Apple", "Mango"];
