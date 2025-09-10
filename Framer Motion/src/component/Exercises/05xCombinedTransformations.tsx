148fe7b96513b6a1f235fd9d8081232652a1716e
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-10 08:11:35 +0530

    exercise 5

--- Framer Motion/src/component/Exercises/05xCombinedTransformations.tsx
+++ Framer Motion/src/component/Exercises/05xCombinedTransformations.tsx
@@ -1,4 +1,12 @@
+import { motion } from "motion/react";
+
 const CombinedTransformations = () => {
-  return <div>CombinedTransformations</div>;
+  return (
+    <motion.div
+      className="box"
+      animate={{ x: 200, scale: 1.5, rotate: 365 }}
+      transition={{ duration: 1 }}
+    />
+  );
 };
 export default CombinedTransformations;
