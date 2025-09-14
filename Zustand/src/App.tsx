2a7ec36882dc931d69f16c93b1514b2a19ac80e2
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-14 17:44:06 +0530

    simple task app

--- Zustand/src/App.tsx
+++ Zustand/src/App.tsx
@@ -1,11 +1,18 @@
 // import Counter from "./componetns/Basics/1.1xSimpleCounter";
-import SeriesApp from "./componetns/Projects/1 Series Vault App/SeriesApp";
+// import SeriesApp from "./componetns/Projects/1 Series Vault App/SeriesApp";
+
+import RscApp from "./roadsideCoder/RscApp";
 
 const App = () => {
   return (
-    <div className="min-h-screen w-full flex justify-center items-center bg-blue-950 text-white">
+    <div>
+      {/*  className="min-h-screen w-full flex justify-center items-center bg-blue-950 text-white" */}
+      {/* From Huxn WebDev */}
       {/* <Counter /> */}
-      <SeriesApp />
+      {/* <SeriesApp /> */}
+
+      {/* From RoadSideCoder */}
+      <RscApp />
     </div>
   );
 };
